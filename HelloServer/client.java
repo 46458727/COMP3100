743bca75d13cffc75d6b8e3c1ef59b6a3ed6ebf6
46458727 <joel.odonnellgoodman@students.mq.edu.au>
2022-03-23 19:57:54 +1100

    Almostschedule and seperate server class

--- HelloServer/client.java
+++ HelloServer/client.java
@@ -2,12 +2,17 @@ import java.net.*;
 import java.io.*;
 
 public class client {
+
+    
+
     private static final String HOST = "127.0.0.1";
     private static final int PORT = 50000;
     private static final String HELO = "HELO\n";
-    //private static final String OK = "OK\n";
+    private static final String OK = "OK\n";
     private static final String AUTH = "AUTH USER\n";
     private static final String REDY = "REDY\n";
+    private static final String GETS = "GETS All\n";
+    //private static final String SCHD = "SCHD";
     private static final String QUIT = "QUIT\n";
 
 
@@ -19,7 +24,8 @@ public class client {
         }
         return dis.readLine();
     }
-
+    
+    
     public static void main(String[] args) throws IOException {
         try {
             Socket sock = new Socket(HOST, PORT);
@@ -30,11 +36,36 @@ public class client {
             System.out.println(serverMsg(dos, HELO, dis));
             System.out.println(serverMsg(dos, AUTH, dis));
             System.out.println(serverMsg(dos, REDY, dis));
+            //get servers
+            String getsNum = serverMsg(dos, GETS, dis);
+            System.out.println(getsNum);
+            
+            String[] getsNumArr = getsNum.split(" ");
+
+            String getsAll = serverMsg(dos, OK, dis);
+            System.out.println(getsAll);
+            String[] getsAllArr = getsAll.split("\n");
+            Server[] serverL = new Server[Integer.parseInt(getsNumArr[1])];
+
+            for (int i = 0; i < getsAllArr.length; i++) {
+                String serverS = dis.readLine();
+                serverL[i] = new Server(serverS.split(" "));
+            }
+            
+            for (Server s : serverL) System.out.println(s);
+
+            /*System.out.println(serverMsg(dos, OK, dis));
+            
+            //Get Jobs
+            String JOBN = serverMsg(dos, REDY, dis);
+            System.out.println(JOBN);
+
+            String[] JOBNARR = JOBN.split(" ",8);*/
+
+
+
+            
             System.out.println(serverMsg(dos, QUIT, dis));
-
-
-            
-            
             sock.close();
         } catch (UnknownHostException e) {
             System.err.println("Unknown Host Exception: " + HOST);
