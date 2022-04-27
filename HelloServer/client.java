b5b209b365c59f6d36be1ded3044e09e933747a4
46458727 <joel.odonnellgoodman@students.mq.edu.au>
2022-04-27 07:27:23 -0700

    smol changes

--- HelloServer/client.java
+++ HelloServer/client.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class client {
     public static void main(String[] args) throws IOException {
+        String[] testArgs = {"-a", "FC"};
+        if (! (Arrays.stream(testArgs).anyMatch("-a"::equals))) return; 
         // open socket to talk with server
         ConnectionManager cManager = new ConnectionManager();
         // creates a jobManager
